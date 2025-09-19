8d339b372391dffe35307314be0297a5394ac6f3
Rogues-Den-Studio <jfurlow91@icloud.com>
2025-09-18 21:33:17 -0500

    Reworked: Movement and Inventory system expanded.
    Reworked the movement of the character controller. Implemented snapshot interpolation over client side prediction. Physics now only runs on the server. We should be able to implement multi-threaded physics later using this method. Client side prediction isn't needed for our slow paced RPG. Inventory is basic but should work. I'm new to Godot so I'm having to learn the UI features before I can get anything like icons in slots and a panel drawn.

--- Scripts/CharacterController.cs
+++ Scripts/CharacterController.cs
@@ -13,17 +13,38 @@ public partial class CharacterController : CharacterBody2D
 
     public long ownerId;
 
+    [Export] public Vector2 serverPosition;
+    [Export] public Vector2 serverVelocity;
     [Export] public float walkSpeed = 175;
     [Export] public float runSpeed = 300;
+    [Export] public float smoothingSpeed = 30f;
 
     public override void _Process(double _delta)
     {
         if (ownerId == 0) return;
+
         CollectInput();
+
+
+        // We are interpolating all client positions here as well as our own.
+        // Later I will look into creating an Area of Interest Management and only interpolate clients we can see.
+        // For now this is works fine. At scale, not so much.
+        // We can interpolate here using the global position because the server only, runs the physics simulation.
+        // Our clients are just visual representations. Later we can enable multithreading and run physics safetly on a seperate thread this way.
+
+        if (serverPosition != Vector2.Zero)
+        {
+            GlobalPosition = GlobalPosition.Lerp(serverPosition, (float)_delta * smoothingSpeed);
+        }
     }
 
     public override void _PhysicsProcess(double _delta)
     {
+        // Here we are using simple snapshot interpolation. The server runs all physics.
+        // Later we can use multi-threaded physics if we keep it this way.
+        // Client side prediction and server reconciliation is overkill for a 2D RPG. A fast-paced FPS is another story.
+        // The small delay of about 50ms keeps it simple and the player movement is smooth.
+
         if (Multiplayer.IsServer()) return;
 
         if (Multiplayer.GetUniqueId() == ownerId)
@@ -33,20 +54,8 @@ public partial class CharacterController : CharacterBody2D
                 //GD.Print($"[Client {ownerId}] Processing {inputQueue.Count} inputs for prediction.");
                 RpcId(1, nameof(ServerReceiveInputBatch), inputQueue.ToArray());
 
-                foreach (byte packedInput in inputQueue)
-                {
-                    stateMachine.OnPhysicsProcess(_delta, packedInput);
-                }
-
                 inputQueue.Clear();
             }
-
-            if (Velocity.Length() > 0)
-            {
-                //GD.Print($"[Peer {Multiplayer.GetUniqueId()}] Applying velocity {Velocity} to node {Name}");
-            }
-
-            MoveAndSlide();
         }
     }
 
@@ -54,9 +63,9 @@ public partial class CharacterController : CharacterBody2D
     {
         // I decided this would be the most efficient way to send inputs to the server.
         // A queue of bytes wont miss any inputs each frame, as collecting in the physics process would.
-        // This should result in about ~620 bytes of bandwith/sec over something like sending raw input each frame.
-        // Sending raw input each frame or physics frame would be about ~2,280 bytes bandwith/sec.
-        // If you need more than 8 inputs you can use a ushort for up to 16 inputs and so on etc.
+        // This should result in about 0.7 bytes of bandwidth/sec per client over something like sending raw input each frame.
+        // Sending raw input each frame or physics frame would be about 2.2 bytes bandwidth/sec per client.
+        // If you need more than 8 inputs you can use a ushort for up to 16 inputs etc.
 
         if (Multiplayer.GetUniqueId() != ownerId || Multiplayer.IsServer()) return;
 
@@ -69,7 +78,13 @@ public partial class CharacterController : CharacterBody2D
         if (Input.IsActionJustPressed("attack")) packedInputs |= (1 << 5);
 
         //GD.Print($"[Client {ownerId}] Collected input: {packedInputs}");
-        inputQueue.Enqueue(packedInputs);
+        //inputQueue.Enqueue(packedInputs);
+
+        if (inputQueue.Count == 0 || inputQueue.Peek() != packedInputs)
+        {
+            if (inputQueue.Count > 0) inputQueue.Clear();
+            inputQueue.Enqueue(packedInputs);
+        }
     }
 
     [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = false, TransferMode = MultiplayerPeer.TransferModeEnum.Unreliable)]
@@ -78,12 +93,15 @@ public partial class CharacterController : CharacterBody2D
         if (Multiplayer.GetRemoteSenderId() != ownerId) return;
 
         //GD.Print($"[Server] Received {inputs.Length} inputs from {ownerId}.");
+
         foreach (byte packedInput in inputs)
         {
-            stateMachine.OnPhysicsProcess(GetProcessDeltaTime(), packedInput);
+            var physicsDelta = GetPhysicsProcessDeltaTime();
+            stateMachine.OnPhysicsProcess(physicsDelta, packedInput);
+            MoveAndSlide();
         }
 
-        MoveAndSlide();
+        serverPosition = GlobalPosition;
     }
 
     [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
